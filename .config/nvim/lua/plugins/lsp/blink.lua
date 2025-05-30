b88c626175cd9cc3f084272a11a72d6ac10e1d52
wangguangyu <wangguangyu@maker-ray.com>
2025-05-30 14:01:12 +0800

    [config] 更新blink.lua配置

--- .config/nvim/lua/plugins/lsp/blink.lua
+++ .config/nvim/lua/plugins/lsp/blink.lua
@@ -10,12 +10,34 @@ return {
         completion = {
             documentation = {
                 auto_show = true
-            }
+            },
+            menu = {
+                draw = {
+                    treesitter = {'lsp',},
+                    columns = { { 'item_idx' }, { 'kind_icon' }, { 'label', 'label_description', gap = 1 } },
+                    components = {
+                        item_idx = {
+                            text = function(ctx) return ctx.idx == 10 and '0' or ctx.idx >= 10 and ' ' or
+                                tostring(ctx.idx) end,
+                            highlight = 'BlinkCmpItemIdx' -- optional, only if you want to change its color
+                        }
+                    }
+                },
+            },
         },
         keymap = {
             -- preset = "super-tab",
             preset = "super-tab",
-
+            ['<A-1>'] = { function(cmp) cmp.accept({ index = 1 }) end },
+            ['<A-2>'] = { function(cmp) cmp.accept({ index = 2 }) end },
+            ['<A-3>'] = { function(cmp) cmp.accept({ index = 3 }) end },
+            ['<A-4>'] = { function(cmp) cmp.accept({ index = 4 }) end },
+            ['<A-5>'] = { function(cmp) cmp.accept({ index = 5 }) end },
+            ['<A-6>'] = { function(cmp) cmp.accept({ index = 6 }) end },
+            ['<A-7>'] = { function(cmp) cmp.accept({ index = 7 }) end },
+            ['<A-8>'] = { function(cmp) cmp.accept({ index = 8 }) end },
+            ['<A-9>'] = { function(cmp) cmp.accept({ index = 9 }) end },
+            ['<A-0>'] = { function(cmp) cmp.accept({ index = 10 }) end },
             -- 默认键- '<C-f>' '<C-b>'
             -- ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
             -- ['<C-b>'] = { 'scroll_documentation_down', 'fallback' },
@@ -24,7 +46,7 @@ return {
             enabled = true
         },
         sources = {
-            default = { "lazydev", "path", "snippets", "buffer", "lsp" },
+            default = { "path", "snippets", "buffer", "lsp", "lazydev"},
             providers = {
                 lazydev = {
                     name = "LazyDev",
