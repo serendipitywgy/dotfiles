1b21f8d248915df771cbfa2a8330f9a33561d1f4
wangguangyu <wangguangyu@maker-ray.com>
2026-02-24 13:30:45 +0800

    update render-markdown

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -2,6 +2,7 @@
 --    这一步会确保插件被下载并加入 runtimepath，但不会自动运行插件逻辑。
 vim.pack.add({
     { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+    { src = "https://github.com/3rd/image.nvim" },
 })
 
 local M = {}
@@ -12,18 +13,37 @@ function M.init()
     if not ok then return end
 
     rm.setup({
-        -- -- 您的配置参数
-        -- render_modes = { 'n', 'c', 'i', 'v' },
-        -- heading = {
-        --     icons = { '   ', '   ', '   ', '   ', '   ', '   ' },
-        -- },
-        -- code = {
-        --     style = 'full',
-        --     width = 'block',
-        -- },
-        -- win_options = {
-        --     conceallevel = { default = vim.o.conceallevel, rendered = 2 },
-        -- },
+        render_modes = { 'n', 'c', 'i', 'v' },
+        heading = {
+            icons = { '   ', '   ', '   ', '   ', '   ', '   ' },
+        },
+        code = {
+            style = 'full',
+            width = 'block',
+        },
+        win_options = {
+            conceallevel = { default = vim.o.conceallevel, rendered = 2 },
+        },
+    })
+
+    -- 配置图像支持
+    require('image').setup({
+        backend = 'kitty',
+        integrations = {
+            markdown = {
+                enabled = true,
+                clear_in_insert_mode = false,
+                download_remote_images = true,
+                only_render_image_at_cursor = false,
+                filetypes = { 'markdown', 'vimwiki' },
+            },
+        },
+        max_width = nil,
+        max_height = nil,
+        max_height_window_percentage = 80,
+        max_width_window_percentage = 80,
+        window_overlap_clear_enabled = true,
+        window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', '' },
     })
 
     -- 在 markdown 文件中，按 <leader>mc (Markdown Code) 插入代码块
