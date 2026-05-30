346c2379b3d8a99e74dcba9c2aabb382d2ad0f68
wangguangyu <wangguangyu@maker-ray.com>
2026-05-30 11:12:38 +0800

    优化inc-rename配置

--- .config/nvim/lua/plugins/inc-rename.lua
+++ .config/nvim/lua/plugins/inc-rename.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     once = true, -- 第一次触发后即可，不必重复 load
     callback = function()
         require("inc_rename").setup({
-            input_buffer_type = "dressing",
+            input_buffer_type = "command",
         })
         vim.keymap.set("n", "<leader>rn", function()
             return ":IncRename " .. vim.fn.expand("<cword>")
